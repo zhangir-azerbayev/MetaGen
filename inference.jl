138d24367b5bafe778151ecfcea8863ba5e5db54
mdb293 <mdb293@cornell.edu>
2019-11-24 11:15:43 -0500

    fixed issue with truncated poisson where it never sampled 1 object

--- inference.jl
+++ inference.jl
@@ -41,19 +41,25 @@ has_output_grad(::TruncatedPoisson) = false
 has_argument_grads(::TruncatedPoisson) = (false,)
 
 @gen function sample_wo_repl(A,n)
+	#now A itself should never change
+	A_mutable = copy(A)
 	A_immutable = copy(A)
 
-	println("A is ", A)
-	println("n is ", n)
+	#println("A_immutable is ", A_immutable)
+	#println("A_mutable is ", A_mutable)
+	#println("n is ", n)
 
     sample = Array{String}(undef,n)
     for i in 1:n
-    	println("i is ", i)
+    	#println("i is ", i)
     	
-    	idx = @trace(Gen.uniform_discrete(1, length(A)), (:idx, i))
-        sample[i] = splice!(A, idx)
-        println("A is ", A)
+    	idx = @trace(Gen.uniform_discrete(1, length(A_mutable)), (:idx, i))
+    	#print("idx is ", idx)
+        sample[i] = splice!(A_mutable, idx)
+        #println("A_mutable is ", A_mutable)
     end
+    #trying to reset A
+    #A = copy(A_immutable)
     #want to rearrange so that the order of items in the sample matches the order of items that we're sampling from
     sampleIdx = names_to_IDs(sample, A_immutable)
     sorted = sort(sampleIdx)
@@ -103,20 +109,21 @@ end
 
 	for j = 1:length(possible_objects_immutable)
 		#set false alarm rate
-		V[j,1] = @trace(Gen.beta(1.909091, 107.99999999999999), (:fa, j)) #leads to false alarm rate of 0.01
+		V[j,1] = @trace(Gen.beta(1.909091, 36.272727), (:fa, j)) #leads to false alarm rate of 0.01
 		#set miss rate
 		V[j,2] = @trace(Gen.beta(1.909091, 36.272727), (:m, j)) #leads to miss rate of 0.05
 	end
 
 	#Determining frame of reality R
 	lambda = 1 #must be <= length of possible_objects
-	low = 1
+	low = 0  #seems that low is never sampled, so this distribution will go from low+1 to high
 	high = length(possible_objects_immutable)
 
-	println("possible_objects_mutable ", possible_objects_mutable)
+	#println("possible_objects_mutable ", possible_objects_mutable)
 
 	numObjects = @trace(trunc_poisson(lambda, low, high), :numObjects)
     R = @trace(sample_wo_repl(possible_objects_mutable,numObjects), :R) #order gets mixed up
+    #why do changes to possible_objects_mutable last?
 
 	#Determing the percept based on the visual system V and the reality frame R
     #A percept is a matrix where each row is the percept for a frame.
@@ -154,8 +161,8 @@ n_frames = 10
 gt_trace,_ = Gen.generate(gm, (possible_objects, n_frames))
 gt_reality,gt_V,gt_percept = Gen.get_retval(gt_trace)
 
-println("gt_reality is ",gt_reality)
-println("gt_percept is ",gt_percept) #could translate back into names
+#println("gt_reality is ",gt_reality)
+#println("gt_percept is ",gt_percept) #could translate back into names
 
 # #Translating gt_percept back into names
 # percept = Matrix{String}(undef, n_frames, length(possible_objects))
@@ -165,11 +172,11 @@ println("gt_percept is ",gt_percept) #could translate back into names
 # println("percept is ",percept)
 
 gt_choices = Gen.get_choices(gt_trace)
-println(gt_choices)
+#println(gt_choices)
 
 
 #get the percepts
-#obs = Gen.get_submap(gt_choices, :percept)
+#obs = Gen.get_submap(gt_choices, :percept
 observations = Gen.choicemap()
 nrows,ncols = size(gt_percept)
 for i = 1:nrows
@@ -179,6 +186,7 @@ for i = 1:nrows
 end
 
 
+
 ##################################################################################################################
 
 #initialize a new trace
@@ -186,15 +194,14 @@ end
 
 #Inference procedure 1: Importance resampling
 
-num_samples = 100
 
 #add log_norm_weights as middle return arguement for importance_sampling
 #(trace, lml_est) = Gen.importance_resampling(gm, (possible_objects, n_frames), observations, num_samples)
 #(traces, log_norm_weights, lml_est) = Gen.importance_sampling(gm, (possible_objects, n_frames), observations, num_samples)
 #for some reason importance_sampling seems not to account for observations
 
-#trying repeated importance_resampling
-# amount_of_computation_per_resample = 100
+# #trying repeated importance_resampling
+# amount_of_computation_per_resample = 10
 # traces = []
 # log_probs = Array{Float64}(undef, num_samples)
 # for i = 1:num_samples
@@ -207,69 +214,26 @@ num_samples = 100
 #################################################################################################################################
 
 #trying Metropolis Hastings
+num_samples = 1000
+amount_of_computation_per_resample = 1 #????
 
+trace,_ = Gen.generate(gm, (possible_objects, n_frames), observations)
 
-# trace, _ = Gen.generate(gm, (possible_objects, n_frames), observations)
-
-# # Perform a single block resimulation update of a trace.
-# function block_resimulation_update(tr)
-
-#     # Block 1: Update the reality
-#     reality = select(:R)
-#     (tr, _) = mh(tr, reality)
-    
-#     # Block 2: Update the visual system
-#     (possible_objects, n_frames) = get_args(tr)
-#     n = length(possible_objects)
-#     for i = 1:n
-#     	row_V = select((:(fa,i)),(:(m,i)))
-#     	(tr, _) = mh(tr, row_V)
-#         #(tr, _) = mh(tr, select(:(fa,i)))
-#         #(tr, _) = mh(tr, select(:(m,i)))
-#     end
-    
-#     # Return the updated trace
-#     tr
-# end;
-
-# function block_resimulation_inference((possible_objects, n_frames), observations)
-#     (tr, _) = generate(gm, (possible_objects, n_frames), observations)
-#     for iter=1:amount_of_computation_per_resample
-#         tr = block_resimulation_update(tr)
-#     end
-#     tr
-# end;
-
-
-# traces = []
-# for i=1:num_samples
-#     tr = block_resimulation_inference((possible_objects, n_frames), observations)
-#     push!(traces,tr)
-# end
-
-
-
-#################################################################################################################################
-
-
-#trying Hamiltonian MC
-
-
-trace, _ = Gen.generate(gm, (possible_objects, n_frames), observations)
-
-#Perform a single block resimulation update of a trace.
+# Perform a single block resimulation update of a trace.
 function block_resimulation_update(tr)
 
     # Block 1: Update the reality
     reality = select(:R)
-    (tr, _) = hmc(tr, reality)
+    (tr, _) = mh(tr, reality)
     
     # Block 2: Update the visual system
     (possible_objects, n_frames) = get_args(tr)
     n = length(possible_objects)
     for i = 1:n
     	row_V = select((:(fa,i)),(:(m,i)))
-    	(tr, _) = hmc(tr, row_V)
+    	(tr, _) = mh(tr, row_V)
+        #(tr, _) = mh(tr, select(:(fa,i)))
+        #(tr, _) = mh(tr, select(:(m,i)))
     end
     
     # Return the updated trace
@@ -292,10 +256,54 @@ for i=1:num_samples
 end
 
 
+
 #################################################################################################################################
 
 
-burnin = 50 #how many samples to ditch
+#trying Hamiltonian MC
+
+
+# trace, _ = Gen.generate(gm, (possible_objects, n_frames), observations)
+
+# # #Perform a single block resimulation update of a trace.
+# function block_resimulation_update(tr)
+
+#     # Block 1: Update the reality
+#     reality = select(:R)
+#     (tr, _) = hmc(tr, reality)
+    
+#     # Block 2: Update the visual system
+#     (possible_objects, n_frames) = get_args(tr)
+#     n = length(possible_objects)
+#     for i = 1:n
+#     	row_V = select((:(fa,i)),(:(m,i)))
+#     	(tr, _) = hmc(tr, row_V)
+#     end
+    
+#     # Return the updated trace
+#     tr
+# end;
+
+# function block_resimulation_inference((possible_objects, n_frames), observations)
+#     (tr, _) = generate(gm, (possible_objects, n_frames), observations)
+#     for iter=1:amount_of_computation_per_resample
+#         tr = block_resimulation_update(tr)
+#     end
+#     tr
+# end;
+
+
+# traces = []
+# for i=1:num_samples
+#     tr = block_resimulation_inference((possible_objects, n_frames), observations)
+#     push!(traces,tr)
+# end
+
+
+#################################################################################################################################
+
+
+burnin = num_samples/2 #how many samples to ditch
 
 realities = Array{String}[]
 Vs = Array{Float64}[]
