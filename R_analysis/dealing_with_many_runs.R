e7d820862a62241e2e84f9e4c0fe8b9eba1cfc16
mdb293 <mdb293@cornell.edu>
2020-05-22 17:05:09 -0400

    made some data processing better and adding exp_MSE to MSE plot

--- R_analysis/dealing_with_many_runs.R
+++ R_analysis/dealing_with_many_runs.R
@@ -3,8 +3,11 @@ library(tidyr)
 library(readr)
 library(tidyboot)
 library(ggplot2)
+library(tidyverse)
 
 source("/Users/marleneberke/Documents/03_Yale/Projects/001_Mask_RCNN/ORB_project3/R_analysis/accuracy.R")
+source("/Users/marleneberke/Documents/03_Yale/Projects/001_Mask_RCNN/ORB_project3/R_analysis/visualize_reality.R")
+source("/Users/marleneberke/Documents/03_Yale/Projects/001_Mask_RCNN/ORB_project3/R_analysis/MSE_Vs.R")
 
 # data <-
 #   list.files(
@@ -22,12 +25,15 @@ source("/Users/marleneberke/Documents/03_Yale/Projects/001_Mask_RCNN/ORB_project
 # }
 # combined_data <- combined_data %>% bind_rows
 
-data <- read_delim("merged.csv",
-                        "&", escape_double = FALSE, trim_ws = TRUE)
-names(data)<-str_replace_all(names(data), c(" " = "."))
+# raw_data <- read_delim("output111.csv",
+#                        "&", escape_double = FALSE, trim_ws = TRUE)
 
-simID <- 1:dim(data)[1]
-data <- cbind(simID, data)
+raw_data <- read_delim("merged.csv",
+                        "&", escape_double = FALSE, trim_ws = TRUE)
+names(raw_data)<-str_replace_all(names(raw_data), c(" " = "."))
+
+simID <- 1:dim(raw_data)[1]
+raw_data <- cbind(simID, raw_data)
 
 ##################################
 
@@ -39,19 +45,30 @@ data <- cbind(simID, data)
 #Row 3478 is messed up. I think these didn't finish running or something
 #and row 3510
 # data <- data[-3478,]
-data <- na.omit(data)
+raw_data <- na.omit(raw_data)
 
-#I <- nrow(data)
-I <- 3477
+#I <- nrow(raw_data)
+I <- 1000 #3477 for partially completed run
 
+combined_data <- map_df(1:I,function(x){return(cbind(simID = x, MSE_Vs(raw_data[x,]), accuracy(raw_data[x,])))})
 
-#Must be a better way to do this
-combined_data <- vector(mode = "list", length = I)
-for(i in 1:I){
-  print(i)
-  combined_data[[i]] <- accuracy(data[i,])
-}
-combined_data <- combined_data %>% bind_rows
+###################################################################
+
+#find noisiest percept
+noisiest_row <- combined_data[which(combined_data$perceived_noise == max(combined_data$perceived_noise)),]
+noisiest_sim <- noisiest_row$simID
+noisiest_percept <- noisiest_row$percept_number
+
+noisiest_df <- raw_data %>% filter(simID == noisiest_sim)
+visualize_reality(noisiest_df) #have to uncomment each graph that I want to see. or put a break point
+#another way
+data <- noisiest_df #then run analysis_of_Vs line by line
+
+#where thresholding gets it wrong
+which(combined_data$A_threshold==0)
+#end noisiest percept
+
+###################################################################
 
 df <-
   combined_data %>% gather(
@@ -64,17 +81,52 @@ df <-
     A_threshold
   )
 
+GetLowerCI <- function(x,y){return(prop.test(x,y)$conf.int[1])}
+GetTopCI <- function(x,y){return(prop.test(x,y)$conf.int[2])}
+
+MyDataFrame <- df %>% group_by(percept_number,Model) %>% summarize(Samples=n(),Hits=sum(Score),Mean=mean(Score),Lower=GetLowerCI(Hits,Samples),Top=GetTopCI(Hits,Samples))
+
 #group_by(percept_number)
-MyDataFrame <- df %>% group_by(percept_number, Model) %>% tidyboot_mean(Score)
+#MyDataFrame <- df %>% group_by(percept_number, Model) %>% tidyboot_mean(Score)
 
 ggplot(
   MyDataFrame,
   aes(
     x = percept_number,
-    y = empirical_stat,
-    ymin = ci_lower,
-    ymax = ci_upper,
+    y = Mean,
+    ymin = Lower,
+    ymax = Top,
     fill = Model,
     group = Model
   )
 ) + geom_ribbon() + geom_line()
+
+###################################################################
+
+df <-
+  combined_data %>% gather(
+    V_param,
+    MSE,
+    MSE_FA,
+    MSE_M,
+    exp_MSE_FA,
+    exp_MSE_M,
+  )
+
+GetMean <- function(x){return(t.test(x)$estimate)}
+GetLowerCI <- function(x){return(t.test(x)$conf.int[1])}
+GetTopCI <- function(x){return(t.test(x)$conf.int[2])}
+
+MyDataFrame <- df %>% group_by(percept_number,V_param) %>% summarize(Mean_MSE=GetMean(MSE),Lower=GetLowerCI(MSE),Top=GetTopCI(MSE))
+
+ggplot(
+  MyDataFrame,
+  aes(
+    x = percept_number,
+    y = Mean_MSE,
+    ymin = Lower,
+    ymax = Top,
+    fill = V_param,
+    group = V_param
+  )
+) + geom_ribbon() + geom_line() + coord_cartesian(ylim = c(0, 0.1)) + coord_cartesian(ylim = c(0, 0.1))
