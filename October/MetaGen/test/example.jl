490c56fe0c2ecd351f1ec33bb6c5914373829d59
mdb293 <mdb293@cornell.edu>
2021-03-24 22:13:12 -0400

    trying to add receptive fields. bug with const Detection2

--- October/MetaGen/test/example.jl
+++ October/MetaGen/test/example.jl
@@ -8,6 +8,12 @@ possible_objects = [1, 2, 3, 4, 5]
 #call it
 #@profilehtml gt_trace,_ = Gen.generate(metacog, (possible_objects,))
 #@profilehtml gt_trace,_ = Gen.generate(metacog, (possible_objects,))
-gt_trace,_ = Gen.generate(metacog, (possible_objects,))
+num_frames = 3
+num_videos = 2
+gt_trace,_ = Gen.generate(metacog, (possible_objects, num_videos, num_frames))
 #println(gt_trace)
 gt_choices = get_choices(gt_trace)
+
+#################################################################################
+num_particles = 100
+#tr = unfold_particle_filter(num_particles);
