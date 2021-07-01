0745f7c87e7f428b9ed00108413b062f9fc2fd45
mdb293 <mdb293@cornell.edu>
2021-07-01 16:28:37 -0400

    fixed bug in setting observations

--- October/MetaGen/scripts/test_cases_from_gm.jl
+++ October/MetaGen/scripts/test_cases_from_gm.jl
@@ -3,7 +3,7 @@ using MetaGen
 using Gen
 
 ################################################################################
-num_videos = 30
+num_videos = 3
 num_frames = 100
 gt_trace,_ = Gen.generate(metacog, (num_videos, num_frames))
 #println(gt_trace)
@@ -18,6 +18,7 @@ end
 receptive_fields = make_receptive_fields()
 num_receptive_fields = length(receptive_fields)
 
+#might be a bug in getting the objects_observed right
 objects_observed = Matrix{Array{Array{Detection2D}}}(undef, num_videos, num_frames)
 camera_trajectories = Matrix{Camera_Params}(undef, num_videos, num_frames)
 for v= 1:num_videos
@@ -25,7 +26,7 @@ for v= 1:num_videos
 		temp = []
 		for rec_field = 1:num_receptive_fields
 			println(gt_choices[:videos => v => :frame_chain => f => rec_field => :observations_2D])
-			vcat(temp, convert(Array{Detection2D}, gt_choices[:videos => v => :frame_chain => f => rec_field => :observations_2D]))
+			temp = vcat(temp, convert(Array{Detection2D}, gt_choices[:videos => v => :frame_chain => f => rec_field => :observations_2D]))
 		end
 		temp_sorted_into_rfs = map(rf -> filter(p -> within(p, rf), temp), receptive_fields)
 		objects_observed[v, f] = temp_sorted_into_rfs
