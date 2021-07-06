5bfd887d25bbcbafde10187ea66f51abf76f7814
mdb293 <mdb293@cornell.edu>
2021-07-06 13:20:50 -0400

    made most @gen function satic. could consider removing receptive fields altogether to remove a for loop

--- October/MetaGen/src/video.jl
+++ October/MetaGen/src/video.jl
@@ -5,10 +5,9 @@ This function takes a category and params and it returns the possible
 objects (as 2D Detections) of that category that could be detected
 """
 #hallucinate objects in 2D image
-@gen function gen_possible_hallucination(params::Video_Params, cat::Int64)
+@gen (static) function gen_possible_hallucination(params::Video_Params, cat::Int64)
     x = @trace(uniform(0,params.image_dim_x), :x)
     y = @trace(uniform(0,params.image_dim_y), :y)
-
     return (x, y, cat)
 end
 possible_hallucination_map = Gen.Map(gen_possible_hallucination)
@@ -18,7 +17,6 @@ function render(params::Video_Params, camera_params::Camera_Params, object_3D::O
     cat = object_3D[4]
     object = Coordinate(object_3D[1], object_3D[2], object_3D[3])
     x, y = get_image_xy(camera_params, params, object)
-
     return (x, y, cat)
 end
 
@@ -28,7 +26,7 @@ end
 Independently samples a camera location and camera focus from a
 uniform distribution
 """
-@gen function gen_camera(params::Video_Params)
+@gen (static) function gen_camera(params::Video_Params)
     #camera location
     camera_location_x = @trace(uniform(params.x_min,params.x_max), :camera_location_x)
     camera_location_y = @trace(uniform(params.y_min,params.y_max), :camera_location_y)
@@ -40,6 +38,13 @@ uniform distribution
     camera_focus_z = @trace(uniform(params.z_min,params.z_max), :camera_focus_z)
 
     camera_params = Camera_Params(Coordinate(camera_location_x,camera_location_y,camera_location_z), Coordinate(camera_focus_x,camera_focus_y,camera_focus_z))
+    return camera_params
+end
+
+@gen function rfs_helper(rfs_vec::Any)#couldn't get the type quite right
+    for i = 1:length(rfs_vec)
+        observations_2D = @trace(rfs(rfs_vec[i]), :observations_2D => i)
+    end
 end
 
 """
@@ -47,7 +52,7 @@ Generates the next frame given the current frame.
 
 state is Array{Any,1}
 """
-@gen function frame_kernel(current_frame::Int64, state, params::Video_Params, v::Matrix{Real}, receptive_fields::Vector{Receptive_Field})
+@gen (static) function frame_kernel(current_frame::Int64, state, params::Video_Params, v::Matrix{Real}, receptive_fields::Vector{Receptive_Field})
 
     ####Update 2D real objects
 
@@ -69,11 +74,8 @@ state is Array{Any,1}
     #for loop over receptive fields
     #@show maximum(map(length, rfs_vec))
     #could re-write with map
-    #@trace(map(rfs)(rfs_vec), :observations_2D)
-
-    for i = 1:length(rfs_vec)
-        observations_2D = @trace(rfs(rfs_vec[i]), (i => :observations_2D))
-    end
+    #@trace(Gen.Map(rfs)(rfs_vec), :observations_2D) #gets no method matching error
+    observations_2D = @trace(rfs_helper(rfs_vec), :observations_2D)
 
     return state #just keep sending the scene / initial state in.
 end
@@ -83,7 +85,7 @@ frame_chain = Gen.Unfold(frame_kernel)
 """
 Samples a new scene.
 """
-@gen function video_kernel(num_frames::Int64, params::Video_Params, v::Matrix{Real}, receptive_fields::Array{Receptive_Field, 1})
+@gen (static) function video_kernel(num_frames::Int64, params::Video_Params, v::Matrix{Real}, receptive_fields::Array{Receptive_Field, 1})
     rfs_element = GeometricElement{Object3D}(params.p_objects, object_distribution, (params,))
     rfs_element = RFSElements{Object3D}([rfs_element]) #need brackets because rfs has to take an array
     init_state = @trace(rfs(rfs_element), :init_scene)
