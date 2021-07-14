6a598842a9a72283ee5d892fac93f65ad84650b3
mdb293 <mdb293@cornell.edu>
2021-07-14 15:47:18 -0400

    conditions on a V, generates percepts. Then conditions on percepts, infers V

--- October/MetaGen/scripts/test_cases_from_gm.jl
+++ October/MetaGen/scripts/test_cases_from_gm.jl
@@ -5,7 +5,7 @@ using PyPlot
 const plt = PyPlot
 using Random
 
-Random.seed!(1234)
+Random.seed!(1235)
 
 ################################################################################
 num_videos = 30
@@ -13,34 +13,45 @@ num_frames = 100
 
 #for testing purposes, let's fix V
 cm = choicemap()
+cm[:init_v_matrix => :lambda_fa => 1 => :fa] = 0.00001
+cm[:init_v_matrix => :lambda_fa => 2 => :fa] = 0.00001
+cm[:init_v_matrix => :lambda_fa => 3 => :fa] = 0.00001
+cm[:init_v_matrix => :lambda_fa => 4 => :fa] = 0.00001
+cm[:init_v_matrix => :lambda_fa => 5 => :fa] = 0.3 #super high false alarm rate for category 5
+cm[:init_v_matrix => :miss_rate => 1 => :miss] = 0.01 #tiny miss rate for category 1
+cm[:init_v_matrix => :miss_rate => 2 => :miss] = 0.99 #huge miss rate for category 2
+cm[:init_v_matrix => :miss_rate => 3 => :miss] = 0.01
+cm[:init_v_matrix => :miss_rate => 4 => :miss] = 0.99
+cm[:init_v_matrix => :miss_rate => 5 => :miss] = 0.01
 for v = 1:num_videos
-	# cm[:videos => v => :v_matrix => :lambda_fa => 1 => :fa] = 0.000001
-	# cm[:videos => v => :v_matrix => :lambda_fa => 2 => :fa] = 0.000001
-	# cm[:videos => v => :v_matrix => :lambda_fa => 3 => :fa] = 0.000001
-	# cm[:videos => v => :v_matrix => :lambda_fa => 4 => :fa] = 0.000001
-	# cm[:videos => v => :v_matrix => :lambda_fa => 5 => :fa] = 0.1 #super high false alarm rate for category 5
-	# cm[:videos => v => :v_matrix => :miss_rate => 1 => :miss] = 0.000001 #tiny miss rate for category 1
-	# cm[:videos => v => :v_matrix => :miss_rate => 2 => :miss] = 0.01
-	# cm[:videos => v => :v_matrix => :miss_rate => 3 => :miss] = 0.01
-	# cm[:videos => v => :v_matrix => :miss_rate => 4 => :miss] = 0.99 #huge miss rate for category 4
-	# cm[:videos => v => :v_matrix => :miss_rate => 5 => :miss] = 0.01
-	for f = 1:num_frames
-		camera_params = camera_trajectories[v, f]
-		cm[:videos => v => :frame_chain => f => :camera => :camera_location_x] = camera_params.camera_location.x
-		cm[:videos => v => :frame_chain => f => :camera => :camera_location_y] = camera_params.camera_location.y
-		cm[:videos => v => :frame_chain => f => :camera => :camera_location_z] = camera_params.camera_location.z
-		cm[:videos => v => :frame_chain => f => :camera => :camera_focus_x] = camera_params.camera_focus.x
-		cm[:videos => v => :frame_chain => f => :camera => :camera_focus_y] = camera_params.camera_focus.y
-		cm[:videos => v => :frame_chain => f => :camera => :camera_focus_z] = camera_params.camera_focus.z
-
-		#for rf = 1:num_receptive_fields
-			#println("objects_observed[v, f][rf] ", objects_observed[v, f][rf])
-			#println("type ", typeof(objects_observed[v, f][rf]))
-		cm[:videos => v => :frame_chain => f => :observations_2D] = convert(Array{Any, 1}, objects_observed[v, f])
-		#end
-	end
-	#def
+	cm[:videos => v => :v_matrix => :lambda_fa => 1 => :fa] = 0.00001
+	cm[:videos => v => :v_matrix => :lambda_fa => 2 => :fa] = 0.00001
+	cm[:videos => v => :v_matrix => :lambda_fa => 3 => :fa] = 0.00001
+	cm[:videos => v => :v_matrix => :lambda_fa => 4 => :fa] = 0.00001
+	cm[:videos => v => :v_matrix => :lambda_fa => 5 => :fa] = 0.3 #super high false alarm rate for category 5
+	cm[:videos => v => :v_matrix => :miss_rate => 1 => :miss] = 0.01 #tiny miss rate for category 1
+	cm[:videos => v => :v_matrix => :miss_rate => 2 => :miss] = 0.99 #huge miss rate for category 2
+	cm[:videos => v => :v_matrix => :miss_rate => 3 => :miss] = 0.01
+	cm[:videos => v => :v_matrix => :miss_rate => 4 => :miss] = 0.99
+	cm[:videos => v => :v_matrix => :miss_rate => 5 => :miss] = 0.01
 end
+# 	for f = 1:num_frames
+# 		camera_params = camera_trajectories[v, f]
+# 		cm[:videos => v => :frame_chain => f => :camera => :camera_location_x] = camera_params.camera_location.x
+# 		cm[:videos => v => :frame_chain => f => :camera => :camera_location_y] = camera_params.camera_location.y
+# 		cm[:videos => v => :frame_chain => f => :camera => :camera_location_z] = camera_params.camera_location.z
+# 		cm[:videos => v => :frame_chain => f => :camera => :camera_focus_x] = camera_params.camera_focus.x
+# 		cm[:videos => v => :frame_chain => f => :camera => :camera_focus_y] = camera_params.camera_focus.y
+# 		cm[:videos => v => :frame_chain => f => :camera => :camera_focus_z] = camera_params.camera_focus.z
+#
+# 		#for rf = 1:num_receptive_fields
+# 			#println("objects_observed[v, f][rf] ", objects_observed[v, f][rf])
+# 			#println("type ", typeof(objects_observed[v, f][rf]))
+# 		cm[:videos => v => :frame_chain => f => :observations_2D] = convert(Array{Any, 1}, objects_observed[v, f])
+# 		#end
+# 	end
+# 	#def
+# end
 
 gt_trace,_ = Gen.generate(main, (num_videos, num_frames), cm)
 #println(gt_trace)
@@ -58,18 +69,17 @@ receptive_fields = make_receptive_fields()
 num_receptive_fields = length(receptive_fields)
 
 #might be a bug in getting the objects_observed right
-objects_observed = Matrix{Array{Array{Detection2D}}}(undef, num_videos, num_frames)
+objects_observed = Matrix{Array{Detection2D}}(undef, num_videos, num_frames)
 camera_trajectories = Matrix{Camera_Params}(undef, num_videos, num_frames)
-for v= 1:num_videos
+for v = 1:num_videos
 	for f = 1:num_frames
-		temp = []
-		for rec_field = 1:num_receptive_fields
-			println(gt_choices[:videos => v => :frame_chain => f => :observations_2D => :observations_2D => rec_field])
-			temp = vcat(temp, convert(Array{Detection2D}, gt_choices[:videos => v => :frame_chain => f => :observations_2D => :observations_2D => rec_field]))
+		temp = gt_choices[:videos => v => :frame_chain => f => :observations_2D]
+		#temp_sorted_into_rfs = map(rf -> filter(p -> within(p, rf), temp), receptive_fields)
+		#objects_observed[v, f] = temp_sorted_into_rfs
+		objects_observed[v, f] = temp
+		if length(temp) > 4
+			println("temp too long ", temp)
 		end
-		temp_sorted_into_rfs = map(rf -> filter(p -> within(p, rf), temp), receptive_fields)
-		objects_observed[v, f] = temp_sorted_into_rfs
-
 		location_x = gt_choices[:videos => v => :frame_chain => f => :camera => :camera_location_x]
 		location_y = gt_choices[:videos => v => :frame_chain => f => :camera => :camera_location_y]
 		location_z = gt_choices[:videos => v => :frame_chain => f => :camera => :camera_location_z]
@@ -80,9 +90,6 @@ for v= 1:num_videos
 		camera_trajectories[v, f] = c
 	end
 end
-
-println("objects_observed ", objects_observed)
-println("camera_trajectories ", camera_trajectories)
 
 #visualize_observations(objects_observed, 1, 1, receptive_fields)
 #visualize_observations(objects_observed, 1, 2, receptive_fields)
@@ -104,48 +111,48 @@ print(file, "\n")
 
 print(file, gt_v, " & ")
 
-num_particles = 10
-traces = unfold_particle_filter(num_particles, objects_observed, camera_trajectories, num_receptive_fields, file)
+num_particles = 2
+traces = unfold_particle_filter(num_particles, objects_observed, camera_trajectories, file)
 println("done")
 #visualize_observations(objects_observed, 1, 1, receptive_fields)
 #visualize_trace(traces, 1, camera_trajectories, 1, 1, params)
 
 close(file)
 
-# Gets v_hat matrix
-best_trace = Gen.get_choices(traces[1])
-v_hat = Matrix{Real}(undef, length(params.possible_objects), 2)
-for j=1:length(params.possible_objects)
-	v_hat[j, 1] = best_trace[:videos => num_videos => :v_matrix => :lambda_fa => j => :fa]
-	v_hat[j, 2] = best_trace[:videos => num_videos => :v_matrix => :miss_rate => j => :miss]
-end
-
-#plot errors of final estimate
-L1_errors = vec(abs.(gt_v - v_hat))
-
-plt.clf()
-plt.title("V matrix L1 errors, $(num_videos) videos, $(num_frames) frames, $(num_particles) particles")
-bins = [0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-plt.hist(L1_errors, bins=bins)
-plt.xlim(0, 1)
-plt.savefig("errors_$(num_videos)_$(num_frames)_$(num_particles).pdf")
-plt.clf()
-
-#plot MSE as function of videos
-MSEs = []
-
-for i=1:num_videos
-	# makes v_hat
-	v_pred = Matrix{Real}(undef, length(params.possible_objects), 2)
-	for j=1:length(params.possible_objects)
-		v_pred[j, 1] = best_trace[:videos => i => :v_matrix => :lambda_fa => j => :fa]
-		v_pred[j, 2] = best_trace[:videos => i => :v_matrix => :miss_rate => j => :miss]
-	end
-	mse = sum((gt_v - v_pred).^2) / (2 * length(params.possible_objects))
-	push!(MSEs, mse)
-end
-
-plt.plot(MSEs)
-plt.title("MSEs $(num_videos) videos, $(num_frames) frames, $(num_particles) particles")
-plt.savefig("mses_$(num_videos)_$(num_frames)_$(num_particles).pdf")
-plt.clf()
+# # Gets v_hat matrix
+# best_trace = Gen.get_choices(traces[1])
+# v_hat = Matrix{Real}(undef, length(params.possible_objects), 2)
+# for j=1:length(params.possible_objects)
+# 	v_hat[j, 1] = best_trace[:videos => num_videos => :v_matrix => :lambda_fa => j => :fa]
+# 	v_hat[j, 2] = best_trace[:videos => num_videos => :v_matrix => :miss_rate => j => :miss]
+# end
+#
+# #plot errors of final estimate
+# L1_errors = vec(abs.(gt_v - v_hat))
+#
+# plt.clf()
+# plt.title("V matrix L1 errors, $(num_videos) videos, $(num_frames) frames, $(num_particles) particles")
+# bins = [0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+# plt.hist(L1_errors, bins=bins)
+# plt.xlim(0, 1)
+# plt.savefig("errors_$(num_videos)_$(num_frames)_$(num_particles).pdf")
+# plt.clf()
+#
+# #plot MSE as function of videos
+# MSEs = []
+#
+# for i=1:num_videos
+# 	# makes v_hat
+# 	v_pred = Matrix{Real}(undef, length(params.possible_objects), 2)
+# 	for j=1:length(params.possible_objects)
+# 		v_pred[j, 1] = best_trace[:videos => i => :v_matrix => :lambda_fa => j => :fa]
+# 		v_pred[j, 2] = best_trace[:videos => i => :v_matrix => :miss_rate => j => :miss]
+# 	end
+# 	mse = sum((gt_v - v_pred).^2) / (2 * length(params.possible_objects))
+# 	push!(MSEs, mse)
+# end
+#
+# plt.plot(MSEs)
+# plt.title("MSEs $(num_videos) videos, $(num_frames) frames, $(num_particles) particles")
+# plt.savefig("mses_$(num_videos)_$(num_frames)_$(num_particles).pdf")
+# plt.clf()
