e0c0fa03f0baa3e5f2775de4d1423aa592a3479b
Zhangir Azerbayev <zazerbayev@gmail.com>
2021-08-05 11:48:45 -0400

    added config file support

--- MetaGen/scripts/parse_JSON.jl
+++ MetaGen/scripts/parse_JSON.jl
@@ -1,16 +1,17 @@
-#println("here")
-#using Pkg
-#Pkg.activate("../../../../../../home/mb2987/ORB_project3/MetaGen/.")
-#println("activated")
+println("here")
 
 using MetaGen
 using JSON
+import YAML
 using Pipe: @pipe
 using Random
 
+config_path = ARGS[1]
+config = YAML.load_file(config_path)
+
 include("useful_functions.jl")
 
-dict = @pipe "../metagen_data/labelled_data_old/0_data_labelled.json" |> open |> read |> String |> JSON.parse
+dict = @pipe config["input_file"] |> open |> read |> String |> JSON.parse
 #dict = @pipe "../../scratch_work_07_16_21/0_data_labelled.json" |> open |> read |> String |> JSON.parse
 #dict = @pipe "../../scratch_work_07_16_21/0_data_labelled.json" |> open |> read |> String |> JSON.parse
 
@@ -20,8 +21,8 @@ Random.seed!(15)
 #outer array is for scenes, then frames, the receptive fields, then last is an array of detections
 
 ################################################################################
-num_videos = 5
-num_frames = 300
+num_videos = config["num_videos"]
+num_frames = config["num_frames"]
 
 params = Video_Params(n_possible_objects = 8)
 
@@ -30,7 +31,7 @@ objects_observed, camera_trajectories = make_observations_office(dict, receptive
 
 ################################################################################
 #Set up the output file
-outfile = string("output.csv")
+outfile = config["output_csv"]
 file = open(outfile, "w")
 
 #file header
@@ -59,15 +60,20 @@ print(file, "\n")
 
 ################################################################################
 #Online MetaGen
-num_particles = 1
+num_particles = config["num_particles"]
+mcmc_steps_outer = config["mcmc_steps_outer"]
+mcmc_steps_inner = config["mcmc_steps_inner"]
 #@profilehtml unfold_particle_filter(false, num_particles, objects_observed, camera_trajectories, params, file)
-traces, inferred_realities, avg_v = unfold_particle_filter(nothing, num_particles, objects_observed, camera_trajectories, params, file)
+traces, inferred_realities, avg_v = unfold_particle_filter(nothing,
+	num_particles, mcmc_steps_outer, mcmc_steps_inner, objects_observed,
+	camera_trajectories, params, file)
 
 println("done with pf for online & retrospective metagen")
 
 ################################################################################
 #Retrospective MetaGen
-unfold_particle_filter(avg_v, num_particles, objects_observed, camera_trajectories, params, file)
+unfold_particle_filter(avg_v, num_particles, mcmc_steps_outer, mcmc_steps_inner,
+	objects_observed, camera_trajectories, params, file)
 
 
 ################################################################################
@@ -75,7 +81,8 @@ unfold_particle_filter(avg_v, num_particles, objects_observed, camera_trajectori
 v = zeros(length(params.possible_objects), 2)
 v[:,1] .= 1.0
 v[:,2] .= 0.5
-unfold_particle_filter(v, num_particles, objects_observed, camera_trajectories, params, file)
+unfold_particle_filter(v, num_particles, mcmc_steps_outer, mcmc_steps_inner,
+	objects_observed, camera_trajectories, params, file)
 
 
 println("done with pf for lesioned metagen")
@@ -89,7 +96,7 @@ close(file)
 out = write_to_dict(dict, camera_trajectories, inferred_realities, num_videos, num_frames)
 
 #open("../../scratch_work_07_16_21/output_tiny_set_detections.json","w") do f
-open("output.json","w") do f
+open(config["output_json"],"w") do f
 	JSON.print(f,out)
 end
 
